af916804f891e37fb36fe2a0a2836b174de76b95
viniciusmr85 <138167608+viniciusmr85@users.noreply.github.com>
2025-03-25 15:58:09 -0300

    Create 7_changes_over_time_analysis.sql

---
+++ scripts/7_changes_over_time_analysis.sql
@@ -0,0 +1,42 @@
+/*
+========================================================================================
+Advanced Data Analytics: Changes Over Time Analysis
+========================================================================================
+- Analyse how a measure evolves over time.
+- Helps track trends and identify seasonality in you data.
+
+	-Total Sales by Year
+
+========================================================================================
+*/
+
+SELECT
+	YEAR(order_date) AS order_year,
+	MONTH(order_date) AS order_month,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+SELECT
+	DATETRUNC(month, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date)
+
+SELECT
+	FORMAT(order_date, 'yyyy-MMM') AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM') 
+ORDER BY FORMAT(order_date, 'yyyy-MMM') 
